8af83c56ee58caefa2dac23d7eeeaa8b9c6f73bf
talishnaider2 <talishnaider@gmail.com>
2024-09-04 16:01:16 +0300

    Added docstring to chain

--- chain.py
+++ chain.py
@@ -1,10 +1,35 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import time
 
 
 class Chain:
+    """
+    Represents a chain of spins with a specified interaction and on-site energy.
+
+    Attributes:
+        N (int): Number of spins in the chain.
+        t (float): Coupling strength between spins.
+        epsilon_r (float): On-site energy.
+        dimension (int): Dimension of the Hilbert space.
+
+        s_x (ndarray): Spin operator for the x direction.
+        s_y (ndarray): Spin operator for the y direction.
+        s_z (ndarray): Spin operator for the z direction.
+        s_plus (ndarray): Spin raising operator.
+        s_minus (ndarray): Spin lowering operator.
+
+    Methods:
+        compute_hamiltonian(): Computes the Hamiltonian of the spin chain.
+    """
+
     def __init__(self, N, t, epsilon_r):
+        """
+        Initializes the Chain with given parameters.
+
+        Args:
+            N (int): Number of sites in the chain.
+            t (float): Hopping parameter.
+            epsilon_r (float): On-site energy.
+        """
         self.N = N
         self.t = t
         self.epsilon_r = epsilon_r
@@ -18,6 +43,12 @@ class Chain:
         self.s_minus = np.array([[0, 0], [1, 0]])  # self.s_x - 1j * self.s_y
 
     def compute_hamiltonian(self):
+        """
+        Computes the Hamiltonian matrix for the chain.
+
+        Returns:
+            ndarray: The Hamiltonian matrix.
+        """
         H = np.zeros((2 ** self.N, 2 ** self.N))
         if self.N < 2:
             return H
@@ -33,7 +64,4 @@ class Chain:
                                                       np.kron(self.s_z + 0.5 * np.eye(2),
                                                               np.eye(2 ** (self.N - (i + 1)))))
             H += on_site_energy
-
-        B = np.sum(np.abs(H - np.conj(H.T)))
-        print("hermitian of H_Chain:", B)
         return H
